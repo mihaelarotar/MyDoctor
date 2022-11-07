98256625783e642ccab9314c127a15a59fae83e1
DariaRadu2001 <dariaradu20@yahoo.com>
2022-11-07 15:09:07 +0200

    Did the UI for home Page

--- app/src/main/java/com/example/mydoctor2/MainActivity.java
+++ app/src/main/java/com/example/mydoctor2/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.mydoctor2;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.example.mydoctor2.data.Sex;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,19 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -47,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.genInput);
+
+        mySpinner.setAdapter(new ArrayAdapter<Sex>(this, android.R.layout.simple_spinner_item, Sex.values()));
+
     }
 
     @Override
--- app/src/main/java/com/example/mydoctor2/data/UserDao.java
+++ app/src/main/java/com/example/mydoctor2/data/UserDao.java
@@ -2,9 +2,6 @@ package com.example.mydoctor2.data;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-import androidx.room.Transaction;
 import androidx.room.Update;
 
 @Dao
